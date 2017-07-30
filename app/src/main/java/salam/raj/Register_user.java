79287b41e08cb0e35640e5f74705608b0a7c95fb
shaiksalam9182 <shaiksalam9182@gmail.com>
2017-07-30 16:50:40 +0530

    creating activities for registration

---
+++ app/src/main/java/salam/raj/Register_user.java
@@ -0,0 +1,13 @@
+package salam.raj;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Register_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
